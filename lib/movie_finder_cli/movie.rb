e1823851e745789a141eac89ac106e26d144498d
Azraf Chowdhury <achowdhur@ltu.edu>
2018-12-19 03:37:09 +0000

    fleshed out movie class, added save feature

--- lib/movie_finder_cli/movie.rb
+++ lib/movie_finder_cli/movie.rb
@@ -1,5 +1,15 @@
-class Movie 
+class MovieFinderCli::Movie 
   
-  attr_accessor :title, :rating, :description
+  attr_accessor :title, :content, :summary, :date, :genre, :director
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end 
+  
+  def save 
+    @@all << self 
+  end 
   
 end 
