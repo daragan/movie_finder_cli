ea8aa4d8ffd5cbd19c065bcb727715e688cbf4a7
Azraf Chowdhury <achowdhur@ltu.edu>
2018-12-19 05:59:43 +0000

    added a menu method, exit option, fleshed out the description for the user clicking for which movie they wanted to see

--- lib/movie_finder_cli/cli_controller.rb
+++ lib/movie_finder_cli/cli_controller.rb
@@ -2,7 +2,6 @@ class MovieFinderCli::Cli_Controller
   
   def call 
     puts "Welcome to Movie finder"
-    puts "Which movie would you like to find out more about?"
     MovieFinderCli::Scraper.new.scrape_movies
     list_movies
   end 
@@ -13,4 +12,30 @@ class MovieFinderCli::Cli_Controller
     end
   end 
   
+   def menu
+    input = ""
+    while input != "exit"
+      puts "Which movie would you like to find out more about?"
+      input = gets.strip
+
+      if input.to_i-1 <= MovieFinderCli::Movie.all.size
+        movie = MovieFinderCli::Movie.all[input.to_i-1]
+
+        puts "Title: #{movie.title}"
+        puts "Genre: #{movie.genre}"
+        puts "Released: #{movie.year}"
+        puts "Synopsis: #{movie.synopsis}"
+
+        puts "Would you like to find out more?"
+        answer = gets.strip
+
+        if ["Y", "YES"].include?(answer.upcase)
+          # how do I get the content for this story?
+          movie.open_in_browser
+        end
+      end
+      puts "Would you like to exit or list again?"
+      input = gets.strip
+    end
+  end
 end 
