a68832b9905ad673876bb71f0d88c7edc096c6f6
Azraf Chowdhury <achowdhur@ltu.edu>
2018-12-19 03:41:15 +0000

    adding more requires to library

--- lib/movie_finder_cli.rb
+++ lib/movie_finder_cli.rb
@@ -6,6 +6,8 @@ require 'pry'
 module MovieFinderCli
   class Error < StandardError; end
   # Your code goes here...
-  
-  
+  require "movie_finder_cli/cli_controller.rb"
+  require "movie_finder_cli/movie.rb"
+  require "movie_finder_cli/scraper.rb"
+  require "movie_finder_cli/version.rb"
 end
