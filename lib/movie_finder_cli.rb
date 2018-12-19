4dee7019d844d95de9685d7dc321666e8892d45e
Azraf Chowdhury <achowdhur@ltu.edu>
2018-12-19 03:33:21 +0000

    added require to movie_finder_cli.rb

--- lib/movie_finder_cli.rb
+++ lib/movie_finder_cli.rb
@@ -1,6 +1,11 @@
 require "movie_finder_cli/version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 module MovieFinderCli
   class Error < StandardError; end
   # Your code goes here...
+  
+  
 end
